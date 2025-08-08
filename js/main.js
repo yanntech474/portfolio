2c2adfce6baec12d3eed175608a8270fb15298fa
yanntech474 <yanntech474@gmail.com>
2025-08-08 15:19:56 +0200

    add .js file

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Navigation bar effects on scroll
+window.addEventListener("scroll",function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+// Services section - Modal
+
+// Portfolio section - Modal
+
+// Our clients - swiper
+
+// website dark/light theme
+
+// Scroll to top button
+
+// Navigation menu item active on page scroll
+
+// responsive navigation menu toggle
+
+// Scroll reveal animations 
+
+// Scroll reveal options to create reaveal animations
+
+
+// Target elements ,and specify options to create reveal animations
+
