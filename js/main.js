3dd8149302d5bb032c057b05471dc07ea203b45c
yanntech474 <yanntech474@gmail.com>
2025-09-03 22:41:14 +0200

    update file

--- js/main.js
+++ js/main.js
@@ -222,10 +222,45 @@ document.addEventListener('DOMContentLoaded', () => {
 // Our clients - swiper
 
 // website dark/light theme
-
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+    localStorage.setItem("saved-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+});
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+if (savedTheme) {
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
 // Scroll to top button
-
+const scrollToTopBtn = document.querySelector(".scrollToTop-btn");
+window.addEventListener("scroll", function () {
+    scrollToTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+scrollToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
 // Navigation menu item active on page scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset; 
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav-items a[href*=" + sectionId + "]").classList.add("active");
+        } else {
+            document.querySelector(".nav-items a[href*=" + sectionId + "]").classList.remove("active");
+        }
+    });
+}); 
 
 // responsive navigation menu toggle
 
